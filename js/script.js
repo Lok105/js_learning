30bddcc596fbb5df31e582b278701509bf03a849
Lok105 <vavdoc@yandex.ru>
2022-06-25 21:42:26 +0300

    First movies script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
-alert('asdadsdas');
+
+const numberOfFilms = +prompt("Сколько фильмоы вы посмотрели?", "");
+
+const personMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  private: false
+};
+
+const a = prompt("Один из последний фильмов", ""),
+    b = prompt("Ваша оценка фильма", ""),
+    c = prompt("Один из последний фильмов", ""),
+    d = prompt("Ваша оценка фильма", "");
+
+personMovieDB.movies[a] = b;
+personMovieDB.movies[c] = d;
+
+console.log(personMovieDB);
