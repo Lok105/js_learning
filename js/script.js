25031356d3587eac99d1d39ad67675a3125d37e9
Lok105 <vavdoc@yandex.ru>
2022-06-26 09:44:21 +0300

    movies cycle

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+'use strict';
 
 const numberOfFilms = +prompt("Сколько фильмоы вы посмотрели?", "");
 
@@ -9,12 +10,28 @@ const personMovieDB = {
   private: false
 };
 
-const a = prompt("Один из последний фильмов", ""),
-    b = prompt("Ваша оценка фильма", ""),
-    c = prompt("Один из последний фильмов", ""),
-    d = prompt("Ваша оценка фильма", "");
+if(personMovieDB.count < 10){
+  alert('Просмотрено довольно мало фильмов');
+}else if(personMovieDB.count >= 10 && personMovieDB.count <= 30){
+  alert('Вы классический зритель');
+}else if(personMovieDB.count > 30){
+  alert('Вы киноман');
+}else{
+  alert('Произошла ошибка');
+}
 
-personMovieDB.movies[a] = b;
-personMovieDB.movies[c] = d;
+for(let i = 1; i < 3; i++){
+
+  const a = prompt("Один из последний фильмов", ""),
+  b = prompt("Ваша оценка фильма", "");
+
+  if(a != null && b != null && a != '' && b != '' && a.length < 50){
+    personMovieDB.movies[a] = b;
+  }else{
+    i--;
+  }
+
+}
+
 
 console.log(personMovieDB);
